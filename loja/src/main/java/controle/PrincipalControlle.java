953c4db581c1c7e131233daf4864405a7cfd2c4f
EliPeixoto <eliane.peixoto@outlook.com>
2023-12-31 12:06:44 -0300

    subindo controle e resources

---
+++ loja/src/main/java/controle/PrincipalControlle.java
@@ -0,0 +1,13 @@
+package controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalControlle {
+
+	@GetMapping("/adm")
+	public String acessarPrincipal(){
+		return "adm/home";
+		}
+}
