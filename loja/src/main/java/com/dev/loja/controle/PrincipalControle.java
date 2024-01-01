5d672bffbdc45337aaaec51993ce74b69f9a6963
EliPeixoto <eliane.peixoto@outlook.com>
2024-01-01 12:41:11 -0300

    organizando pastas

--- loja/src/main/java/com/dev/loja/controle/PrincipalControle.java
+++ loja/src/main/java/com/dev/loja/controle/PrincipalControle.java
@@ -1,10 +1,11 @@
-package controle;
+package com.dev.loja.controle;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PrincipalControlle {
+public class PrincipalControle {
 
 	@GetMapping("/adm")
 	public String acessarPrincipal(){
